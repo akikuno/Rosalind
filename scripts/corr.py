6de2f21fea2732646fbfd1cf439a34e627b9b658
akikuno <akikuno@@users.noreply.github.com>
2022-04-05 10:46:38 +0900

    update

--- scripts/corr.py
+++ scripts/corr.py
@@ -43,51 +43,96 @@ if not data_case == []:
 
 _, seq = fmtfa(data)
 
+seq_rev = [revcomp(s) for s in seq]
 
-def hamming_pos(seq1, seq2):
-    pos = []
+seq_all = seq + seq_rev
+import collections
+
+from numpy import hamming
+
+C = collections.Counter(seq_all)
+num = sum(1 for c in C.values() if c == 1) // 2
+
+seq_mut = []
+for k,v in C.items():
+    if v == 1 and num > 0:
+        seq_mut.append(k)
+        num -= 1
+
+
+def hamming_snv(seq1, seq2):
+    dist = 0
+    pos = 0
+    nt = ""
     for idx, (s1, s2) in enumerate(zip(seq1, seq2)):
         if s1 != s2:
-            pos.append(idx)
-    return pos
+            dist += 1
+            pos = idx
+            nt = s2
+    return dist, pos, nt
 
-N = len(seq)
 
-# seq1= "TGGAATCGGAAATCTCTCTGAAAATTTTCTTCTGTTGGAAGATACTTCTT"
-seq_mut = []
-for i in range(N):
-    seq_diff = 0
-    pos = list()
-    for j in range(N):
-        seq1 = seq[i]
-        seq2 = seq[j]
-        x1 = hamming_pos(seq1, seq2)
-        x2 = hamming_pos(seq1, revcomp(seq2))
-        if len(x1) == 1:
-            pos.append(x1[0])
-        if len(x2) == 1:
-            pos.append(x2[0])
-    if len(pos) == 1:
-        seq_mut.append((seq1, pos[0]))
-
-set(seq_mut)
-
-ans = set()
+seq_mut_set = set(seq_mut)
+seq_correct = []
 for seq1 in seq_mut:
     for seq2 in seq:
-        if hamming_dist(seq1, seq2) == 1:
-            for idx, (s1, s2) in enumerate(zip(seq1, seq2)):
-                if s1 != s2:
-                    nt_index = [s2, idx]
-        elif hamming_dist(seq1, revcomp(seq2)) == 1:
-            for idx, (s1, s2) in enumerate(zip(seq1, revcomp(seq2))):
-                if s1 != s2:
-                    nt_index = [s2, idx]
-    seq_correct = list(seq1)
-    nt, idx = nt_index
-    seq_correct[idx] = nt
-    seq_correct = "".join(seq_correct)
-    ans.add(f"{seq1}->{seq_correct}")
+        if seq2 in seq_mut_set:
+            continue
+        dist, pos, nt = hamming_snv(seq1, seq2)
+        if dist == 1:
+            s = list(seq1)
+            s[pos] = nt
+            seq_correct.append("".join(s))
+            break
+        dist, pos, nt = hamming_snv(seq1, revcomp(seq2))
+        if dist == 1:
+            s = list(seq1)
+            s[pos] = nt
+            seq_correct.append("".join(s))
+            break
+
+ans = []
+for seq1, seq2 in zip(seq_mut, seq_correct):
+    ans.append(f"{seq1}->{seq2}")
+
+
+# N = len(seq)
+
+# # seq1= "TGGAATCGGAAATCTCTCTGAAAATTTTCTTCTGTTGGAAGATACTTCTT"
+# seq_mut = []
+# for i in range(N):
+#     seq_diff = 0
+#     pos = list()
+#     for j in range(N):
+#         seq1 = seq[i]
+#         seq2 = seq[j]
+#         x1 = hamming_pos(seq1, seq2)
+#         x2 = hamming_pos(seq1, revcomp(seq2))
+#         if len(x1) == 1:
+#             pos.append(x1[0])
+#         if len(x2) == 1:
+#             pos.append(x2[0])
+#     if len(pos) == 1:
+#         seq_mut.append((seq1, pos[0]))
+
+# set(seq_mut)
+
+# ans = set()
+# for seq1 in seq_mut:
+#     for seq2 in seq:
+#         if hamming_dist(seq1, seq2) == 1:
+#             for idx, (s1, s2) in enumerate(zip(seq1, seq2)):
+#                 if s1 != s2:
+#                     nt_index = [s2, idx]
+#         elif hamming_dist(seq1, revcomp(seq2)) == 1:
+#             for idx, (s1, s2) in enumerate(zip(seq1, revcomp(seq2))):
+#                 if s1 != s2:
+#                     nt_index = [s2, idx]
+#     seq_correct = list(seq1)
+#     nt, idx = nt_index
+#     seq_correct[idx] = nt
+#     seq_correct = "".join(seq_correct)
+#     ans.add(f"{seq1}->{seq_correct}")
 
 # def mut_index(seq1, seq2):
 #     revseq2 = revcomp(seq2)
