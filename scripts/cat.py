7015dc63e493d870e5789e99f2ee523a9b1f5ab9
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-05-02 08:02:20 +0900

    update

--- scripts/cat.py
+++ scripts/cat.py
@@ -17,12 +17,15 @@ def fmtfa(fasta: list):
     return header, seq
 
 
+def revcomp(seq: str):
+    conv = {"A": "T", "G": "C", "C": "G", "T": "A"}
+    return "".join([conv[s] for s in seq[::-1]])
+
+
 # INPUT -------------------------------------------
 
 file_in = "sample/dataset/cat.txt"
 file_out = "sample/output/cat.txt"
-
-# file_in = "case/dataset/cat.txt"
 
 with open(file_in) as f:
     data = f.read().splitlines()
@@ -30,13 +33,64 @@ with open(file_in) as f:
 with open(file_out) as f:
     outcome = f.read().splitlines()
 
+file_in = "case/dataset/cat.txt"
+
+with open(file_in) as f:
+    data_case = f.read().splitlines()
+
+if not data_case == []:
+    data = data_case
 
 # MAIN -------------------------------------------
 
 
+_, seq = fmtfa(data)
+
+seq = seq[0]
+
+
+def countRNA2Structures(seq):
+    if seq in cache:
+        return cache[seq]
+    tmp = []
+    for k in range(1, len(seq), 2):
+        tmp.append(countRNA2Structures(seq[1:k]) * cache[seq[0] + seq[k]] * countRNA2Structures(seq[k + 1 :]))
+    # assign current sequence into dictionary for later use
+    cache[seq] = sum(tmp)
+    return cache[seq]
+
+
+# set up initial dictionary for number of matches for the sequence
+cache = {
+    "": 1,
+    "A": 0,
+    "C": 0,
+    "G": 0,
+    "U": 0,
+    "AA": 0,
+    "AC": 0,
+    "AG": 0,
+    "AU": 1,
+    "CA": 0,
+    "CC": 0,
+    "CG": 1,
+    "CU": 0,
+    "GA": 0,
+    "GC": 1,
+    "GG": 0,
+    "GU": 0,
+    "UA": 1,
+    "UC": 0,
+    "UG": 0,
+    "UU": 0,
+}
+
+ans = countRNA2Structures(seq) % 10**6
+
+ans = str(ans)
 # OUTPUT -------------------------------------------
 
 with open("case/output/cat.txt", "w") as f:
-    f.write()
+    f.write(ans)
 
 # END
--- scripts/kmp.py
+++ scripts/kmp.py
@@ -17,12 +17,15 @@ def fmtfa(fasta: list):
     return header, seq
 
 
+def revcomp(seq: str):
+    conv = {"A": "T", "G": "C", "C": "G", "T": "A"}
+    return "".join([conv[s] for s in seq[::-1]])
+
+
 # INPUT -------------------------------------------
 
 file_in = "sample/dataset/kmp.txt"
 file_out = "sample/output/kmp.txt"
-
-# file_in = "case/dataset/kmp.txt"
 
 with open(file_in) as f:
     data = f.read().splitlines()
@@ -30,13 +33,40 @@ with open(file_in) as f:
 with open(file_out) as f:
     outcome = f.read().splitlines()
 
+file_in = "case/dataset/kmp.txt"
+
+with open(file_in) as f:
+    data_case = f.read().splitlines()
+
+if not data_case == []:
+    data = data_case
 
 # MAIN -------------------------------------------
 
+_, seq = fmtfa(data)
+seq = seq[0]
+
+seqrev = seq[::-1]
+
+ans = []
+
+for i in range(len(seq)):
+    tmp = 0
+    for j in range(i, len(seq)):
+        print(i, j, seqrev[j])
+        if seqrev[j] != seq[0]:
+            continue
+        # if seq[: j - i] != seqrev[i:j][::-1]:
+        #     break
+        if seq[: j - i] == seqrev[i:j][::-1]:
+            tmp = j - i
+    ans.append(tmp)
+
+ans = " ".join(map(str, ans[::-1]))
 
 # OUTPUT -------------------------------------------
 
 with open("case/output/kmp.txt", "w") as f:
-    f.write()
+    f.write(ans)
 
 # END
