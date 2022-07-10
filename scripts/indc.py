43c54991970f659d206119a8b3cbaaee234aa01b
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-07-11 06:05:42 +0900

    update

--- scripts/indc.py
+++ scripts/indc.py
@@ -17,12 +17,15 @@ def fmtfa(fasta: list):
     return header, seq
 
 
+def revcomp(seq: str):
+    conv = {"A": "T", "G": "C", "C": "G", "T": "A"}
+    return "".join([conv[s] for s in seq[::-1]])
+
+
 # INPUT -------------------------------------------
 
 file_in = "sample/dataset/indc.txt"
 file_out = "sample/output/indc.txt"
-
-# file_in = "case/dataset/indc.txt"
 
 with open(file_in) as f:
     data = f.read().splitlines()
@@ -30,13 +33,34 @@ with open(file_in) as f:
 with open(file_out) as f:
     outcome = f.read().splitlines()
 
+file_in = "case/dataset/indc.txt"
+
+with open(file_in) as f:
+    data_case = f.read().splitlines()
+
+if not data_case == []:
+    data = data_case
 
 # MAIN -------------------------------------------
 
+import math
+
+N = int(data[0])
+
+math.comb(10, 3) * (1 / 2) ** 10
+
+for n in range(0, 2 * N + 1):
+    x = math.comb(2 * N, n) * (1 / 2) ** (2 * n)
+    math.log10(x)
+
+math.comb(2 * N, 0)
+math.log10((1 / 2) ** (2 * 0.01))
+
+ans = ""
 
 # OUTPUT -------------------------------------------
 
 with open("case/output/indc.txt", "w") as f:
-    f.write()
+    f.write(ans)
 
 # END
